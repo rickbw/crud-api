87ad5186b1bbded2393ad7c9f12d47407d9620de
Rick Warren <rickbw@users.noreply.github.com>
2013-06-27 09:23:33 -0700

    Added retry adapters for set and delete, just like get

---
+++ src/main/java/rickbw/crud/adapter/RetryAsyncMapResourceSetter.java
@@ -0,0 +1,63 @@
+package rickbw.crud.adapter;
+
+import java.io.IOException;
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import rickbw.crud.async.AsyncMapResourceSetter;
+import rickbw.retry.RetrierFuture;
+
+
+public final class RetryAsyncMapResourceSetter<KEY, RSRC, RESPONSE>
+implements AsyncMapResourceSetter<KEY, RSRC, RESPONSE> {
+
+    private final AsyncMapResourceSetter<? super KEY, ? super RSRC, RESPONSE> delegateSetter;
+    private final int maxRetries;
+
+
+    public RetryAsyncMapResourceSetter(
+            final AsyncMapResourceSetter<? super KEY, ? super RSRC, RESPONSE> delegate,
+            final int maxRetries) {
+        this.delegateSetter = Preconditions.checkNotNull(delegate);
+
+        Preconditions.checkArgument(maxRetries >= 0, "Negative retries");
+        this.maxRetries = maxRetries;
+    }
+
+    @Override
+    public ListenableFuture<RESPONSE> putAsync(final KEY key, final RSRC resource) {
+        // Make first getFuture() call here so async activity starts now:
+        final ListenableFuture<RESPONSE> future = this.delegateSetter.putAsync(key, resource);
+        // Wrap in retrier in case original Future throws:
+        final ListenableFuture<RESPONSE> retryFuture = new RetrierFuture<RESPONSE>(future, new FutureSetter(key, resource), this.maxRetries);
+        return retryFuture;
+    }
+
+    @Override
+    public void close(final RESPONSE resource) throws IOException {
+        this.delegateSetter.close(resource);
+    }
+
+
+    private final class FutureSetter implements Function<Integer, ListenableFuture<RESPONSE>> {
+        @Nullable
+        private final KEY key;
+        @Nullable
+        private final RSRC resource;
+
+        public FutureSetter(@Nullable final KEY key, @Nullable final RSRC resource) {
+            this.key = key;
+            this.resource = resource;
+        }
+
+        @Override
+        public ListenableFuture<RESPONSE> apply(final Integer retryIndex) {
+            return RetryAsyncMapResourceSetter.this.delegateSetter.putAsync(this.key, this.resource);
+        }
+    }
+
+}
