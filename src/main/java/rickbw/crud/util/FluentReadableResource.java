e8a6b9c07791ab3e330c733a5db3b69203c5753d
Rick Warren <rickbw@users.noreply.github.com>
2014-03-14 16:33:43 -0700

    Implemented FluentReadableResource.equals() and hashCode()

--- src/main/java/rickbw/crud/util/FluentReadableResource.java
+++ src/main/java/rickbw/crud/util/FluentReadableResource.java
@@ -25,42 +25,105 @@ import rx.functions.Func1;
  */
 public abstract class FluentReadableResource<RSRC> implements ReadableResource<RSRC> {
 
+    /*package*/ final ReadableResource<?> delegate;
+
+
     public static <RSRC> FluentReadableResource<RSRC> from(final ReadableResource<RSRC> resource) {
         if (resource instanceof FluentReadableResource<?>) {
             return (FluentReadableResource<RSRC>) resource;
         } else {
-            Preconditions.checkNotNull(resource);
-            return new FluentReadableResource<RSRC>() {
-                @Override
-                public Observable<RSRC> get() {
-                    return resource.get();
-                }
-
-                // TODO: override equals() and hashCode()
-            };
+            return new DelegatingReadableResource<>(resource);
         }
     }
 
     public <TO> FluentReadableResource<TO> mapValue(final Func1<? super RSRC, ? extends TO> mapper) {
-        Preconditions.checkNotNull(mapper, "null function");
-        return new FluentReadableResource<TO>() {
-            @Override
-            public Observable<TO> get() {
-                final Observable<? extends RSRC> observable = outerResource().get();
-                final Observable<TO> mapped = observable.map(mapper);
-                return mapped;
-            }
-
-            // TODO: override equals() and hashCode()
-        };
+        return new MappingReadableResource<>(this, mapper);
     }
 
     // TODO: Expose other Observable methods
 
     // TODO: Adapt Subscriber
 
-    private FluentReadableResource<RSRC> outerResource() {
-        return this;
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FluentReadableResource<?> other = (FluentReadableResource<?>) obj;
+        if (!this.delegate.equals(other.delegate)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        final int result = prime + this.delegate.hashCode();
+        return result;
+    }
+
+    protected FluentReadableResource(final ReadableResource<?> delegate) {
+        this.delegate = Preconditions.checkNotNull(delegate, "null delegate");
+    }
+
+
+    private static final class DelegatingReadableResource<RSRC>
+    extends FluentReadableResource<RSRC> {
+        public DelegatingReadableResource(final ReadableResource<RSRC> delegate) {
+            super(delegate);
+        }
+
+        @Override
+        public Observable<RSRC> get() {
+            @SuppressWarnings("unchecked")
+            final ReadableResource<RSRC> rsrc = (ReadableResource<RSRC>) super.delegate;
+            return rsrc.get();
+        }
+    }
+
+
+    private static final class MappingReadableResource<FROM, TO> extends FluentReadableResource<TO> {
+        private final Func1<? super FROM, ? extends TO> mapper;
+
+        public MappingReadableResource(
+                final ReadableResource<? extends FROM> delegate,
+                final Func1<? super FROM, ? extends TO> mapper) {
+            super(delegate);
+            this.mapper = Preconditions.checkNotNull(mapper, "null function");
+        }
+
+        @Override
+        public Observable<TO> get() {
+            @SuppressWarnings("unchecked")
+            final ReadableResource<FROM> rsrc = (ReadableResource<FROM>) super.delegate;
+            final Observable<FROM> observable = rsrc.get();
+            final Observable<TO> mapped = observable.map(this.mapper);
+            return mapped;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (!super.equals(obj)) {
+                return false;
+            }
+            final MappingReadableResource<?, ?> other = (MappingReadableResource<?, ?>) obj;
+            return this.mapper.equals(other.mapper);
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = super.hashCode();
+            result = prime * result + this.mapper.hashCode();
+            return result;
+        }
     }
 
 }
