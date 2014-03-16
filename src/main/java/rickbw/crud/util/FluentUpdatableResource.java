d3eda6b9413b8291a9db506f8dca4d53eda9fee1
Rick Warren <rickbw@users.noreply.github.com>
2014-03-16 13:10:41 -0700

    Implemented FluentUpdatableResource.equals() and hashCode()

--- src/main/java/rickbw/crud/util/FluentUpdatableResource.java
+++ src/main/java/rickbw/crud/util/FluentUpdatableResource.java
@@ -25,6 +25,9 @@ import rx.functions.Func1;
  */
 public abstract class FluentUpdatableResource<UPDATE, RESPONSE> implements UpdatableResource<UPDATE, RESPONSE> {
 
+    protected final UpdatableResource<?, ?> delegate;
+
+
     public static <UPDATE, RESPONSE> FluentUpdatableResource<UPDATE, RESPONSE> from(
             final UpdatableResource<? super UPDATE, RESPONSE> resource) {
         if (resource instanceof FluentUpdatableResource<?, ?>) {
@@ -33,51 +36,145 @@ public abstract class FluentUpdatableResource<UPDATE, RESPONSE> implements Updat
             return result;
         } else {
             Preconditions.checkNotNull(resource);
-            return new FluentUpdatableResource<UPDATE, RESPONSE>() {
-                @Override
-                public Observable<RESPONSE> update(final UPDATE update) {
-                    return resource.update(update);
-                }
-            };
+            return new DelegatingUpdatableResource<>(resource);
         }
     }
 
     public <TO> FluentUpdatableResource<UPDATE, TO> mapResponse(
             final Func1<? super RESPONSE, ? extends TO> mapper) {
-        Preconditions.checkNotNull(mapper, "null function");
-
-        return new FluentUpdatableResource<UPDATE, TO>() {
-            @Override
-            public Observable<TO> update(final UPDATE update) {
-                final Observable<? extends RESPONSE> observable = outerResource().update(update);
-                final Observable<TO> mapped = observable.map(mapper);
-                return mapped;
-            }
-        };
+        return new MappingUpdatableResource<>(this, mapper);
     }
-
-    // TODO: Expose other Observable methods
 
     public <TO> FluentUpdatableResource<TO, RESPONSE> adaptUpdate(
             final Func1<? super TO, ? extends UPDATE> adapter) {
-        Preconditions.checkNotNull(adapter, "null function");
-
-        return new FluentUpdatableResource<TO, RESPONSE>() {
-            @Override
-            public Observable<RESPONSE> update(final TO update) {
-                final UPDATE transformed = adapter.call(update);
-                final Observable<RESPONSE> observable = outerResource().update(transformed);
-                return observable;
-            }
-        };
+        return new AdaptingUpdatableResource<>(this, adapter);
     }
 
     // TODO: Transform Subscriber
 
-    // TODO: override equals() and hashCode()
+    // TODO: Expose other Observable methods
 
-    private FluentUpdatableResource<UPDATE, RESPONSE> outerResource() {
-        return this;
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FluentUpdatableResource<?, ?> other = (FluentUpdatableResource<?, ?>) obj;
+        if (!this.delegate.equals(other.delegate)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        final int result = prime + this.delegate.hashCode();
+        return result;
+    }
+
+    protected FluentUpdatableResource(final UpdatableResource<?, ?> delegate) {
+        this.delegate = Preconditions.checkNotNull(delegate, "null delegate");
+    }
+
+
+    private static final class DelegatingUpdatableResource<UPDATE, RESPONSE>
+    extends FluentUpdatableResource<UPDATE, RESPONSE> {
+        public DelegatingUpdatableResource(final UpdatableResource<? super UPDATE, RESPONSE> delegate) {
+            super(delegate);
+        }
+
+        @Override
+        public Observable<RESPONSE> update(final UPDATE update) {
+            @SuppressWarnings("unchecked")
+            final UpdatableResource<? super UPDATE, RESPONSE> rsrc
+                    = (UpdatableResource<? super UPDATE, RESPONSE>) super.delegate;
+            return rsrc.update(update);
+        }
+    }
+
+
+    private static final class MappingUpdatableResource<UPDATE, FROM, TO>
+    extends FluentUpdatableResource<UPDATE, TO> {
+        private final Func1<? super FROM, ? extends TO> mapper;
+
+        private MappingUpdatableResource(
+                final UpdatableResource<UPDATE, FROM> delegate,
+                final Func1<? super FROM, ? extends TO> mapper) {
+            super(delegate);
+            this.mapper = Preconditions.checkNotNull(mapper, "null function");
+        }
+
+        @Override
+        public Observable<TO> update(final UPDATE update) {
+            @SuppressWarnings("unchecked")
+            final UpdatableResource<UPDATE, FROM> rsrc = (UpdatableResource<UPDATE, FROM>) super.delegate;
+            final Observable<? extends FROM> observable = rsrc.update(update);
+            final Observable<TO> mapped = observable.map(this.mapper);
+            return mapped;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (!super.equals(obj)) {
+                return false;
+            }
+            final MappingUpdatableResource<?, ?, ?> other = (MappingUpdatableResource<?, ?, ?>) obj;
+            return this.mapper.equals(other.mapper);
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = super.hashCode();
+            result = prime * result + this.mapper.hashCode();
+            return result;
+        }
+    }
+
+
+    private static final class AdaptingUpdatableResource<FROM, TO, RESPONSE>
+    extends FluentUpdatableResource<TO, RESPONSE> {
+        private final Func1<? super TO, ? extends FROM> adapter;
+
+        private AdaptingUpdatableResource(
+                final UpdatableResource<FROM, RESPONSE> delegate,
+                final Func1<? super TO, ? extends FROM> adapter) {
+            super(delegate);
+            this.adapter = Preconditions.checkNotNull(adapter, "null function");
+        }
+
+        @Override
+        public Observable<RESPONSE> update(final TO update) {
+            final FROM transformed = this.adapter.call(update);
+            @SuppressWarnings("unchecked")
+            final UpdatableResource<FROM, RESPONSE> rsrc = (UpdatableResource<FROM, RESPONSE>) super.delegate;
+            final Observable<RESPONSE> observable = rsrc.update(transformed);
+            return observable;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (!super.equals(obj)) {
+                return false;
+            }
+            final AdaptingUpdatableResource<?, ?, ?> other = (AdaptingUpdatableResource<?, ?, ?>) obj;
+            return this.adapter.equals(other.adapter);
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = super.hashCode();
+            result = prime * result + this.adapter.hashCode();
+            return result;
+        }
     }
 
 }
