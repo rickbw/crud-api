19efc8048ac3edc5e1b7b5caeb7b2919770f4cff
Rick Warren <rickbw@users.noreply.github.com>
2014-04-17 09:39:55 -0700

    Added more TextLineFileResource tests

--- src/test/java/rickbw/crud/file/ReadableTextLineFileResourceTest.java
+++ src/test/java/rickbw/crud/file/ReadableTextLineFileResourceTest.java
@@ -1,22 +1,46 @@
+/* Copyright 2013–2014 Rick Warren
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package rickbw.crud.file;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Iterator;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
 
+import rickbw.crud.ReadableResource;
 import rickbw.crud.ReadableResourceTest;
+import rx.Observable;
 
 
+/**
+ * Test of the {@link ReadableResource} aspect of {@link TextLineFileResource}.
+ */
 public class ReadableTextLineFileResourceTest extends ReadableResourceTest<String> {
 
     private static final ImmutableList<String> lines = ImmutableList.of(
             "foo=bar bax=quux",
-            "fish=dog answer=42");
+            "hello, world");
 
     private File file;
 
@@ -29,6 +53,22 @@ public class ReadableTextLineFileResourceTest extends ReadableResourceTest<Strin
                 writer.write(line + '\n');
             }
         }
+    }
+
+    @Test
+    public void fileContentsExpected() {
+        // given:
+        final TextLineFileResource resource = createDefaultResource();
+
+        // when:
+        final Observable<String> result = resource.get();
+
+        // then:
+        final Iterator<String> actualLines = result.toBlockingObservable().getIterator();
+        for (final String expectedLine : lines) {
+            assertEquals(expectedLine, actualLines.next());
+        }
+        assertFalse(actualLines.hasNext());
     }
 
     @Override
---
+++ src/test/java/rickbw/crud/file/UpdatableTextLineFileResourceTest.java
@@ -0,0 +1,80 @@
+/* Copyright 2013–2014 Rick Warren
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package rickbw.crud.file;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import rickbw.crud.UpdatableResource;
+import rickbw.crud.UpdatableResourceTest;
+import rx.Observable;
+
+
+/**
+ * Test of the {@link UpdatableResource} aspect of {@link TextLineFileResource}.
+ */
+public class UpdatableTextLineFileResourceTest extends UpdatableResourceTest<String, Void> {
+
+    private File file;
+
+
+    @Before
+    public void setup() throws IOException {
+        this.file = File.createTempFile(getClass().getSimpleName(), null);
+    }
+
+    @Test
+    public void linesWritten() throws IOException {
+        // given:
+        final UpdatableResource<String, Void> resource = createDefaultResource();
+        final String expectedLine = createDefaultUpdate();
+        final int numLines = 3;
+
+        // when:
+        for (int i = 0; i < numLines; ++i) {
+            final Observable<Void> response = resource.update(expectedLine);
+            final Void responseValue = response.toBlockingObservable().single();
+            assertNull(responseValue);  // only possible value of Void
+        }
+
+        // then:
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.file))) {
+            for (int i = 0; i < numLines; ++i) {
+                final String actualLine = reader.readLine();
+                assertEquals(expectedLine, actualLine);
+            }
+            assertNull(reader.readLine());
+        }
+    }
+
+    @Override
+    protected TextLineFileResource createDefaultResource() {
+        return new TextLineFileResource(this.file);
+    }
+
+    @Override
+    protected String createDefaultUpdate() {
+        return "hello, world";
+    }
+
+}
