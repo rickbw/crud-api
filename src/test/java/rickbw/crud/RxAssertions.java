d962c1be34f2b88efc6a6a7776bcb112a8824017
Rick Warren <rickbw@users.noreply.github.com>
2014-06-11 09:18:30 -0700

    Expanded HttpResource unit tests (in crud-http); added supporting stuff here

--- src/test/java/rickbw/crud/RxAssertions.java
+++ src/test/java/rickbw/crud/RxAssertions.java
@@ -18,12 +18,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Assert;
 
 import com.google.common.collect.Lists;
 
 import rx.Observable;
+import rx.Observer;
 
 
 /**
@@ -49,6 +52,168 @@ public final class RxAssertions {
                 .toBlockingObservable()
                 .toIterable());
         assertEquals(expectedValues, actualValues);
+    }
+
+    public static <T> void subscribeWithOnNextFailure(final Observable<T> obs)
+    throws InterruptedException {
+        // given:
+        final String success = "success";
+        final String failed = "failed";
+        final AtomicReference<String> onNextCalled = new AtomicReference<>(failed);
+        final AtomicReference<String> onCompleteNotCalled = new AtomicReference<>(success);
+        final AtomicReference<String> onErrorCalled = new AtomicReference<>(failed);
+
+        subscribeAndWait(obs, 2, new Observer<T>() {
+            @Override
+            public void onNext(final T t) {
+                onNextCalled.set(success);
+                throw new IllegalStateException("mock exception");
+            }
+
+            @Override
+            public void onCompleted() {
+                onCompleteNotCalled.set(failed);
+            }
+
+            @Override
+            public void onError(final Throwable e) {
+                onErrorCalled.set(success);
+            }
+        });
+
+        // then:
+        assertEquals(success, onNextCalled.get());
+        assertEquals(success, onCompleteNotCalled.get());
+        assertEquals(success, onErrorCalled.get());
+    }
+
+    public static <T> void subscribeWithOnCompletedFailure(final Observable<T> obs)
+    throws InterruptedException {
+        // given:
+        final String success = "success";
+        final String failed = "failed";
+        final AtomicReference<String> onNextCalled = new AtomicReference<>(failed);
+        final AtomicReference<String> onCompleteCalled = new AtomicReference<>(failed);
+        final AtomicReference<String> onErrorCalled = new AtomicReference<>(failed);
+
+        subscribeAndWait(obs, 3, new Observer<T>() {
+            @Override
+            public void onNext(final T t) {
+                onNextCalled.set(success);
+            }
+
+            @Override
+            public void onCompleted() {
+                onCompleteCalled.set(success);
+                throw new IllegalStateException("mock exception");
+            }
+
+            @Override
+            public void onError(final Throwable e) {
+                onErrorCalled.set(success);
+            }
+        });
+
+        // then:
+        assertEquals(success, onNextCalled.get());
+        assertEquals(success, onCompleteCalled.get());
+        assertEquals(success, onErrorCalled.get());
+    }
+
+    public static <T> void subscribeWithOnNextAndOnErrorFailures(final Observable<T> obs)
+    throws InterruptedException {
+        // given:
+        final String success = "success";
+        final String failed = "failed";
+        final AtomicReference<String> onNextCalled = new AtomicReference<>(failed);
+        final AtomicReference<String> onCompleteNotCalled = new AtomicReference<>(success);
+        final AtomicReference<String> onErrorCalled = new AtomicReference<>(failed);
+
+        subscribeAndWait(obs, 2, new Observer<T>() {
+            @Override
+            public void onNext(final T t) {
+                onNextCalled.set(success);
+                throw new IllegalStateException("trigger onError");
+            }
+
+            @Override
+            public void onCompleted() {
+                onCompleteNotCalled.set(failed);
+            }
+
+            @Override
+            public void onError(final Throwable e) {
+                onErrorCalled.set(success);
+                throw new IllegalStateException("mock exception");
+            }
+        });
+
+        // then:
+        assertEquals(success, onNextCalled.get());
+        assertEquals(success, onCompleteNotCalled.get());
+        assertEquals(success, onErrorCalled.get());
+    }
+
+    public static <T> void subscribeWithOnCompletedAndOnErrorFailures(final Observable<T> obs)
+    throws InterruptedException {
+        // given:
+        final String success = "success";
+        final String failed = "failed";
+        final AtomicReference<String> onNextCalled = new AtomicReference<>(failed);
+        final AtomicReference<String> onCompleteCalled = new AtomicReference<>(failed);
+        final AtomicReference<String> onErrorCalled = new AtomicReference<>(failed);
+
+        subscribeAndWait(obs, 3, new Observer<T>() {
+            @Override
+            public void onNext(final T t) {
+                onNextCalled.set(success);
+            }
+
+            @Override
+            public void onCompleted() {
+                onCompleteCalled.set(success);
+                throw new IllegalStateException("trigger onError");
+            }
+
+            @Override
+            public void onError(final Throwable e) {
+                onErrorCalled.set(success);
+                throw new IllegalStateException("mock exception");
+            }
+        });
+
+        // then:
+        assertEquals(success, onNextCalled.get());
+        assertEquals(success, onCompleteCalled.get());
+        assertEquals(success, onErrorCalled.get());
+    }
+
+    public static <T> void subscribeAndWait(
+            final Observable<T> observable,
+            final int expectedObserverCalls,
+            final Observer<T> observer) throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(expectedObserverCalls);
+        final Observer<T> wrapperObserver = new Observer<T>() {
+            @Override
+            public void onNext(final T t) {
+                latch.countDown();
+                observer.onNext(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                latch.countDown();
+                observer.onCompleted();
+            }
+
+            @Override
+            public void onError(final Throwable e) {
+                latch.countDown();
+                observer.onError(e);
+            }
+        };
+        observable.subscribe(wrapperObserver);
+        latch.await();
     }
 
     private RxAssertions() {
